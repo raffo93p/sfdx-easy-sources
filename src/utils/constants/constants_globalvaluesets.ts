0b021c47049563e1843d352424eed6c7497e9cbe
Raffaele Prudenzano <raffo93p@hotmail.it>
2022-11-22 12:27:56 +0100

    fix on globalvalueset isActive

--- src/utils/constants/constants_globalvaluesets.ts
+++ src/utils/constants/constants_globalvaluesets.ts
@@ -4,7 +4,7 @@ export const GVSETS_EXTENSION = ".globalValueSet-meta.xml";
 export const GVSETS_ROOT_TAG = "GlobalValueSet";
 
 export const GVSET_ROOT = 'customValue';
-export const GVSET_HEADER = ['fullName', 'default', 'label'];
+export const GVSET_HEADER = ['fullName', 'default', 'isActive', 'label'];
 export const GVSET_KEY = 'fullName';
 
 export const GVSET_ITEMS = {
