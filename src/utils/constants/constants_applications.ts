3a54f01d7b8caf1d743dffe524ce9e56d5649882
Raffaele Prudenzano <raffo93p@hotmail.it>
2023-06-27 10:55:33 +0200

    fix quotes profileActionOverride

--- src/utils/constants/constants_applications.ts
+++ src/utils/constants/constants_applications.ts
@@ -1,10 +1,11 @@
+import { setDefault } from "../utils";
 import { DEFAULT_SFXML_PATH } from "./constants";
 
 export const APPLICATIONS_EXTENSION = ".app-meta.xml";
 export const APPLICATIONS_ROOT_TAG = "CustomApplication";
 
 export const PAO_ROOT = 'profileActionOverrides';
-export const PAO_HEADER = ['actionName', 'content', 'formFactor','pageOrSobjectType','recordType','type','profile'];
+export const PAO_HEADER = ['actionName', 'content', 'formFactor','pageOrSobjectType', setDefault('recordType'),'type','profile'];
 export const PAO_KEY = ['recordType', 'content', 'profile','actionName','formFactor'];
 
 export const APPLICATION_ITEMS = {
